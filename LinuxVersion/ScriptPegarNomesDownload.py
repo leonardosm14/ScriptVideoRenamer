e3574832facce1a95e6fb10882f09aa6ddaaf285
Leonardo de Sousa Marques <141475587+leonardosm14@users.noreply.github.com>
2024-03-08 15:21:00 -0300

    Create ScriptPegarNomesDownload.py

---
+++ LinuxVersion/ScriptPegarNomesDownload.py
@@ -0,0 +1,20 @@
+import os
+
+#Diretório
+#src = r'DIRETORIO-DA-PASTA'
+src = '/home/apv-065/ScriptNomes/ScriptVideoRenamer'
+
+#O primeiro passo é pegar os nomes de todos arquivos .mp4 na pasta
+def listarNomes(src):
+  
+    listaNomes = []
+  
+    for arquivo in os.listdir(src):
+        if arquivo.endswith('.mp4'):
+            listaNomes.append(arquivo)
+          
+    with open('NomesDownload.txt', 'w') as f:
+        for nome in listaNomes:
+            f.write(nome + '\n')
+          
+    return
